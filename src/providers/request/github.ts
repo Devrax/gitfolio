2ecfe26c2530f349096dbf3609502ae70a8517d4
Rafael Mejia <devrafx@gmail.com>
2021-01-25 16:17:54 -0400

    fix: Remove Authorization api

--- src/providers/request/github.ts
+++ src/providers/request/github.ts
@@ -3,22 +3,16 @@ import { IGithubUser, IGithubUserRepo } from "../../models/github.models";
 import { GET } from "./http-client";
 
 const APIURL = 'https://api.github.com';
-const APIKEY = '2b1d51b8421b2c0f0c4cdabf052ff257b573d9b5';
 const USER = 'Devrax';
 const STOREUSER = ref<IGithubUser | null>(null);
 const STOREREPOS = ref<IGithubUserRepo[] | null>(null);
 
-const headers: Headers = new Headers({ 
-    Authorization: `token ${APIKEY}`, 
-    'content-type': 'application/json; charset=utf-8'
-});
+const USERINFORMATION = GET<IGithubUser>(`${APIURL}/users/${USER}`);
 
-const USERINFORMATION = GET<IGithubUser>(`${APIURL}/users/${USER}`, { headers });
-
-const USERREPOS = GET<IGithubUserRepo[]>(`${APIURL}/users/${USER}/repos`, { headers });
+const USERREPOS = GET<IGithubUserRepo[]>(`${APIURL}/users/${USER}/repos`);
 
 const REPOLANGUAGES = (api: string) => {
-    return GET<{[props:string]: number}>(api, { headers });
+    return GET<{[props:string]: number}>(api);
 }
 
 export { 
